35161d7040274c7f6fdd2c26a520e7186c41a636
Melih Korkmaz <mk@count.ly>
2018-08-03 16:37:19 +0300

    Fixed config

--- src/config.js
+++ src/config.js
@@ -8,6 +8,6 @@ if(process.env.NODE_ENV==="development"){
     module.exports = {
         port : PORT || configs.port,
         mongodb_connection : MONGODB_CONNECTION || configs.mongodb_connection,
-        mongodb_database : MONGODB_CONNECTION || configs.mongodb_database
+        mongodb_database : MONGODB_DATABASE || configs.mongodb_database
     };
 }
